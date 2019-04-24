e286fb40561996625dc0c00ec09f6c2fabbb3616
Coamo <37449956+Coamoo@users.noreply.github.com>
2019-04-24 21:07:50 +0200

    Added mutex and fixed some bugs

--- main.c
+++ main.c
@@ -11,8 +11,10 @@
 #define TL 1
 #define CR 2
 #define SEX 3
-#define K 10 //Klienci
+#define K 6 //Klienci
 #define M 2  //Szafki
+#define maxx(a,b) (a>b?a:b)
+
 
 typedef struct _LOCAL_QUEUE
 {
@@ -25,6 +27,7 @@ int rank, size, T = 0, TLast = 0, recivedMessages = 0;
 
 LOCAL_QUEUE Queue[K];
 int msg[MSG_SIZE];
+int msg2[MSG_SIZE];
 MPI_Status status;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -61,20 +64,23 @@ int isQueueEmpty()
 
 void entryTheCloakroom()
 {
+    pthread_mutex_lock( &mutex );
     TLast = T;
     Queue[rank].TLast = T;
     Queue[rank].cloakroom = chooseCloakroom();
-    msg[ID] = rank;
-    msg[TL] = TLast;
-    msg[CR] = Queue[rank].cloakroom;
-    msg[SEX] = Queue[rank].type;
+    pthread_mutex_unlock( &mutex );
+    msg2[ID] = rank;
+    msg2[TL] = TLast;
+    msg2[CR] = Queue[rank].cloakroom;
+    msg2[SEX] = Queue[rank].type;
     for (int i = 0; i < size; i++)
     {
         if (i == rank)
         {
             continue;
         }
-        MPI_Send(msg, MSG_SIZE, MPI_INT, i, MSG_CR_REQUEST, MPI_COMM_WORLD);
+        MPI_Send(msg2, MSG_SIZE, MPI_INT, i, MSG_CR_REQUEST, MPI_COMM_WORLD);
+        sleep(1);
     }
 }
 
@@ -83,7 +89,8 @@ int countPeopleInCloakroom()
     int numberOfPeople = 0;
     for (int i = 0; i < size; i++)
     {
-        if (Queue[i].cloakroom == Queue[rank].cloakroom && Queue[i].TLast < TLast && i != rank)
+        printf("Jestem %d ta sama szatnia: %d, TLast: %d, tlast ten sam %d\n", rank,Queue[i].cloakroom == Queue[rank].cloakroom, Queue[i].TLast < Queue[rank].TLast,i < rank && Queue[i].TLast == Queue[rank].TLast);
+        if (Queue[i].cloakroom == Queue[rank].cloakroom && (Queue[i].TLast < Queue[rank].TLast || (i < rank && Queue[i].TLast == Queue[rank].TLast)) && i != rank)
         {
             numberOfPeople++;
         }
@@ -108,43 +115,50 @@ int isOtherSexInCloakroom()
 {
     for (int i = 0; i < size; i++)
     {
-        if (Queue[i].cloakroom == Queue[rank].cloakroom  && Queue[i].TLast < TLast && i != rank && Queue[i].type != Queue[rank].type)
+        if (Queue[i].cloakroom == Queue[rank].cloakroom  && (Queue[i].TLast < TLast || (i < rank && Queue[i].TLast == TLast)) && i != rank && Queue[i].type != Queue[rank].type)
         {
             return 1;
-        }pthread_mutex_lock
+        }
     }
     return 0;
 }
 
 void receiveAllAcceptances()
 {
+    int a,b,c;
     while(1)
     {
-        MPI_Recv(msg, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MSG_RESPONSE, MPI_COMM_WORLD, &status);
-        int id_j = msg[ID];
-        Queue[id_j].TLast = msg[TL];
-        Queue[id_j].type = msg[SEX];
-        Queue[id_j].cloakroom = msg[CR];
-        if(!(isOtherSexInCloakroom()) && countPeopleInCloakroom() < M && countPeopleInCloakroom() == size))
+        MPI_Recv(msg2, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MSG_RESPONSE, MPI_COMM_WORLD, &status);
+        pthread_mutex_lock( &mutex );
+        int id_j = msg2[ID];
+        Queue[id_j].TLast = msg2[TL];
+        Queue[id_j].type = msg2[SEX];
+        Queue[id_j].cloakroom = msg2[CR];
+        if(!(a = isOtherSexInCloakroom()) && (b=countPeopleInCloakroom()) < M && (c = queueSize()) == size)
         {
+            printf("Inna płec: %d, osoby: %d\n",a,b);
             break;
         }
+        pthread_mutex_unlock( &mutex );
+        sleep(1);
     }
 }
 
 void sendInformationToEveryone()
 {
-    msg[ID] = rank;
-    msg[TL] = T;
-    msg[CR] = -2;
-    msg[SEX] = Queue[rank].type;
+    msg2[ID] = rank;
+    msg2[TL] = T;
+    msg2[CR] = -2;
+    msg2[SEX] = Queue[rank].type;
     for (int i = 0; i < size; i++)
     {
         if (i == rank)
         {
             continue;
         }
-        MPI_Send(msg, MSG_SIZE, MPI_INT, i, MSG_RESPONSE, MPI_COMM_WORLD);
+        printf("[SITE] Jestem %d wysyłam do %d\n",rank,i);
+        MPI_Send(msg2, MSG_SIZE, MPI_INT, i, MSG_RESPONSE, MPI_COMM_WORLD);
+        sleep(1);
     }
 }
 
@@ -154,7 +168,7 @@ void *receiver(void *arg)
     {
         MPI_Recv(msg, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MSG_CR_REQUEST, MPI_COMM_WORLD, &status);
         pthread_mutex_lock( &mutex );
-        T = max(T, msg[TL]) + 1;
+        T = maxx(T, msg[TL]) + 1;
         int id_j = msg[ID];
         if (isQueueEmpty())
         {
@@ -172,18 +186,23 @@ void *receiver(void *arg)
         }
         pthread_mutex_unlock( &mutex );
         MPI_Send(msg, MSG_SIZE, MPI_INT, id_j, MSG_RESPONSE, MPI_COMM_WORLD);
+        sleep(1);
     }
 }
 
-void *sender(void *arg);
+void *sender(void *arg)
 {
     while (1)
     {
         entryTheCloakroom();
+        printf("Wysyłam zapytania o wejście %d, %d\n", rank, TLast);
         receiveAllAcceptances();
-        goChangeAndSwim();
+        printf("Dostąłem wszystkie pozwolenia %d wchodzę do szatni %d\n", rank,Queue[rank].cloakroom);
+        goChangeAndSwim(rank);
         sendInformationToEveryone();
         emptyTheQueue();
+        printf("Oczekuje na kolejke %d",rank);
+        sleep(rand()%5);
     }
 }
 
@@ -214,7 +233,7 @@ int main(int argc, char **argv)
     }
     else
     {
-        printf("Do term przyszło za mało klientów, %d", size);
+        printf("Do term przyszło za mało klientów, %d\n", size);
     }
 
     MPI_Finalize();
